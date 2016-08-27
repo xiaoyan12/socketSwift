5b88fabb496f25a8587ab882bb34baca8b6fb5f3
dorayo <yomingyo@gmail.com>
2016-08-27 12:12:14 +0800

    tianjialeyilei

---
+++ 01socket套接字/XYViewController.swift
@@ -0,0 +1,35 @@
+//
+//  XYViewController.swift
+//  01socket套接字
+//
+//  Created by 闫世超 on 16/8/27.
+//  Copyright © 2016年 河南青云信息技术有限公司. All rights reserved.
+//
+
+import UIKit
+
+class XYViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+   
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
